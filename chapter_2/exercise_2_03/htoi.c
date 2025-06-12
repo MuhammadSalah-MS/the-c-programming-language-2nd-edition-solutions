a985094021b6bc73ea5a2c2274a7e3a016956ed1
Muhammad Salah <67175615+MuhammadSalah-MS@users.noreply.github.com>
2025-06-12 12:07:18 +0300

    Update htoi.c
    simple and clearer version

--- chapter_2/exercise_2_03/htoi.c
+++ chapter_2/exercise_2_03/htoi.c
@@ -1,79 +1,34 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 #include <ctype.h>
 
-#define MAXLINE 100
-
-int get_line(char line[], int lim);
-int htoi(char hex[]);
-
-int main(void)
-{
-  char hex[MAXLINE];
-
-  get_line(hex, MAXLINE);
-
-  printf("%d", htoi(hex));
-
-  return 0;
+int htoi(const char s[]);
+int main() {
+    char x[]="0xFFA234\0";
+    printf("%ld",htoi(x));
+    return 0;
 }
 
-int get_line(char line[], int lim)
-{
-  char c;
-  int i = 0;
-  while (i < lim - 1 && (c = getchar()) != EOF && c != '\n')
-  {
-    line[i++] = c;
-  }
+/* htoi: convert hexadecimal string s to integer */
+int htoi(const char s[]) {
+    int i = 0;
+    int n = 0;
+    int hexdigit;
 
-  line[i] = '\0';
+    if (s[i] == '0' && (s[i + 1] == 'x' || s[i + 1] == 'X')) {
+        i = 2;
+    }
 
-  return i;
+    while (s[i] != '\0') {
+        if (isdigit(s[i])) {
+            hexdigit = s[i] - '0';
+        } else if (s[i] >= 'a' && s[i] <= 'f') {
+            hexdigit = s[i] - 'a' + 10;
+        } else if (s[i] >= 'A' && s[i] <= 'F') {
+            hexdigit = s[i] - 'A' + 10;
+        }
+
+        n = 16 * n + hexdigit;
+        i++;
+    }
+    return n;
 }
-
-int htoi(char hex[])
-{
-  int result = 0;
-
-  int i = 0, len = strlen(hex);
-
-  while (i < len)
-  {
-    if (hex[i] == '0' && (hex[i + 1] == 'x' || hex[i + 1] == 'X'))
-    {
-      i += 2;
-    }
-
-    int temp = tolower(hex[i]);
-
-    if (isdigit(temp))
-    {
-      temp -= 48;
-    }
-
-    if (isalpha(temp) && temp <= 'f')
-    {
-      temp = temp - 'a' + 10;
-    }
-
-    if ((hex[i] >= '0' && hex[i] <= '9') || (hex[i] >= 'a' && hex[i] <= 'f') || (hex[i] >= 'A' && hex[i] <= 'F'))
-    {
-      result += temp * (int)pow(16, len - i - 1);
-    }
-    else
-    {
-      printf("Error: Not a valid hex value.\n Try this format: 0xHHHH, where H is a hex digit.\n");
-    }
-
-    ++i;
-  }
-
-  return result;
-}
-
-// NOTE: The conversion algorithm from hex to dec is very similar with the
-// conversion algorithm from bin to dec, but the base is not 2 but 16.
-// The general formula is: x1*B^N + x2*B^(N - 1) + ... + xn*B^(N - N), where B
-// is the base from we convert to dec, in this case B = 16.
